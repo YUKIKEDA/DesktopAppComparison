472c449770fbe666a800f50530387f97cf3bfd58
YUKIKEDA <yuuuu1008ki@gmail.com>
2025-12-29 19:42:47 +0900

    Initialize Avalonia ToDo application with essential structure and components
    - Created the foundational structure for the Avalonia ToDo application, including App.axaml and App.axaml.cs for application setup.
    - Implemented MainWindow and its ViewModel to manage the main interface and data context.
    - Added ViewLocator for dynamic view resolution based on view models.
    - Established project configuration in ToDoApp.Avalonia.csproj, including necessary package references.
    - Included application manifest for Windows compatibility and added an application icon.
    - Introduced basic greeting functionality in MainWindowViewModel for user interaction.

---
+++ ToDoApp.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace ToDoApp.Avalonia.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ ToDoApp.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ToDoApp.Avalonia.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
